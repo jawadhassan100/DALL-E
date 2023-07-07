29e4e713665c0ff8f466b12839c04448d7bb8395
Jawad Hassan <jawadhassankhan2001@gmail.com>
2023-07-07 11:36:56 +0500

    new changes

--- server/index.js
+++ server/index.js
@@ -9,16 +9,20 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:[],
+  methods:[],
+  credentials:true
+}));
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
-  res.status(200).json({
-    message: "Hello from DALL.E!",
-  });
+  res.json(
+    "Hello from DALL.E!"
+  );
 });
 
 const startServer = async () => {
